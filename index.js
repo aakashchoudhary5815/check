b1adc7a408d9db175c2cd4857ac843445ad036a0
Aakash Choudhary <52340133+aakashchoudhary5815@users.noreply.github.com>
2024-03-26 20:23:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use("/api/posts", postRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-connect(process.env.MONGO_URI)
+connect(process.env.MONGO_URI, { bufferCommands: false })
   .then(
     app.listen(process.env.PORT || 5000, () => {
       console.log(`Server running on port ${process.env.PORT}`);
